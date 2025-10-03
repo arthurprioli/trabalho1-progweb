41e8bc95f4a1c54b232673d7a49d73279b472c92
Arthur Reigada Prioli Menezes <arthur.prioli@gmail.com>
2025-10-03 11:36:17 -0300

    Adiciona mais allowed hosts

--- listador_jogos/listador_jogos/settings.py
+++ listador_jogos/listador_jogos/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-k10i^h=aotf)6(hmm51l)gliu2*v(mu#a&^aabyxeno2(v))$^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0',]
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', '127.0.0.1:8000']
 
 
 # Application definition
